d9839fca742b321cab0605786b17ec4b9c1956ff
Bar Goldenstein <bargolden12@gmail.com>
2023-10-12 16:37:44 +0300

    tweaked layers

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,11 @@ import PanToLocation from './components/PanToLocation';
 
 import APIAccess from './services/ApiAccess';
 
-function MapLayer({ name, url, subdomains, checked=false }) {
+function MapLayer({ name, url, subdomains, checked = false }) {
     return (
         <LayersControl.BaseLayer name={name} checked={checked}>
             <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url={url} subdomains={subdomains || ['a', 'b', 'c']} />
+                url={url} subdomains={subdomains?.split('') || ['mt0', 'mt1', 'mt2', 'mt3']} />
         </LayersControl.BaseLayer>
     );
 }
@@ -76,20 +76,10 @@ export default function App() {
     return (
 		<MapContainer center={position} zoom={7} style={{ height: '100vh' }}>
 			<LayersControl position="bottomleft">
-				<MapLayer name="Default" checked
-					subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
-					url='https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}' />
-
-				<MapLayer name="Leaflet"
-					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
-
-				<MapLayer name="Terrain"
-					subdomains={['mt0', 'mt1','mt2','mt3']}
-					url='https://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}' />
-
-				<MapLayer name="Satellite"
-					subdomains={['mt0', 'mt1','mt2','mt3']}
-					url='https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}' />
+				<MapLayer name="Default"   		 url='https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}' checked 		   />
+				<MapLayer name="Open Street Map" url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' subdomains='abc' />
+				<MapLayer name="Terrain"   		 url='https://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}'   			   />
+				<MapLayer name="Satellite" 		 url='https://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}' 			   />
 
 				<LayersControl.Overlay name='Dark Mode'><LayerGroup /></LayersControl.Overlay>
 
