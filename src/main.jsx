73c7d29386464d06b213af6b64eabb2bc913ae69
Bar Goldenstein <bargolden12@gmail.com>
2024-01-14 23:29:13 +0200

    fixed oversight

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import APIAccess from './services/APIAccess';
 
+
+APIAccess.initCollections();
 createRoot(document.getElementById('root')).render(<App />);
