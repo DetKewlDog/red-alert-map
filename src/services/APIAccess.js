52c0b7213ff57087174d34cc9d98d297afdaadd1
DetKewlDog <bargolden12@gmail.com>
2024-01-15 11:27:03 +0200

    reverted dev endpoint

--- src/services/APIAccess.js
+++ src/services/APIAccess.js
@@ -35,7 +35,7 @@ class APIAccess {
 
   static async getRedAlerts() {
     APIAccess.initCollections();
-    return await axios.get(`${BACKEND_URL}/dev/all/19`, args)
+    return await axios.get(`${BACKEND_URL}/realtime`, args)
       .then(result => result.data?.data?.map(i => i.split(', ')[0]))
   }
 
