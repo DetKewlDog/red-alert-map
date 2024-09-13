c2edfd982d99406aa01219e5287bb28b9e947f1c
Bar Goldenstein <bargolden12@gmail.com>
2024-09-13 22:57:08 +0300

    debug toast

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { AlertView } from './components/AlertView';
 import useAlertDisplay from './hooks/UseAlertDisplay';
 import { AlertFetcher } from './types';
 import React from 'react';
+import { Toast } from 'primereact/toast';
+import { isUsingMobile } from './util/IsUsingMobile';
 
 export default function App() {
 	const [alertFetcher, setAlertFetcher] = React.useState<AlertFetcher>(
@@ -14,10 +16,17 @@ export default function App() {
 	);
 	const { alertedCities } = useAlertDisplay(alertFetcher);
 	const [loaded, setLoaded] = React.useState(false);
+	const toast = React.useRef<Toast>(null);
 
 	React.useEffect(() => {
 		APIAccess.initCollections().then(() => {
 			setLoaded(true);
+			toast.current?.show({
+				severity: 'success',
+				summary: 'loaded',
+				detail: 'loaded collections',
+				life: 3000
+			});
 		});
 	}, []);
 
@@ -25,24 +34,33 @@ export default function App() {
 		
 	}, [alertFetcher]);
 
-	if (!loaded) {
-		return (
-			<div style={{
-				'display': 'flex',
-				'justifyContent': 'center',
-				'alignContent': 'center',
-				'height': '100vh',
-				'backgroundColor': document.body.getAttribute('theme') === 'dark' ? 'var(--dm-bg1)' : 'var(--wm-bg1)'
-			}}>
-				<ProgressSpinner style={{ 'margin': 'auto' }} />
-			</div>
-		)
-	}
-
 	return (
 		<>
-			<AlertView alertFetcher={alertFetcher} alertedCities={alertedCities}  />
-			<UILayer setAlertFetcher={setAlertFetcher} alertedCities={alertedCities} />
+			<Toast ref={toast} position='top-center' pt={{
+        root: {
+          style: {
+            paddingTop: isUsingMobile() ? '32px': '8px',
+            paddingInline: '16px',
+            pointerEvents: 'none'
+          }
+        }
+			}} />
+			{loaded ? (
+				<>
+					<AlertView alertFetcher={alertFetcher} alertedCities={alertedCities}  />
+					<UILayer setAlertFetcher={setAlertFetcher} alertedCities={alertedCities} />
+				</>
+			) : (
+				<div style={{
+					display: 'flex',
+					justifyContent: 'center',
+					alignContent: 'center',
+					height: '100vh',
+					backgroundColor: document.body.getAttribute('theme') === 'dark' ? 'var(--dm-bg1)' : 'var(--wm-bg1)'
+				}}>
+					<ProgressSpinner style={{ 'margin': 'auto' }} />
+				</div>
+			)}
 		</>
 	);
 };
